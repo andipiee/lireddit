ba3fe535e1f7a77147bdbf94678dd4ffb86f6a19
Andi Nurul Widi Prastyawan <andipiee@Andis-MacBook-Air.local>
2022-04-17 00:24:25 +0800

    init orm but still get an error

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,24 @@
-console.log("Hello there");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const constants_1 = require("./constants");
+const Post_1 = require("./entitites/Post");
+const main = async () => {
+    const orm = await core_1.MikroORM.init({
+        entities: [Post_1.Post],
+        user: "andipiee",
+        password: "star13star",
+        allowGlobalContext: true,
+        dbName: "lireddit",
+        type: "postgresql",
+        debug: !constants_1.__prod__
+    });
+    const post = orm.em.fork({}).create(Post_1.Post, { title: "my first post" });
+    await orm.em.persistAndFlush(post);
+    console.log("---------slq 2----------");
+    await orm.em.nativeInsert(Post_1.Post, { title: "my first post" });
+};
+main().catch((err) => {
+    console.log(err);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello there");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entitites/Post";
+
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        user: "andipiee",
+        password: "star13star",
+        allowGlobalContext: true,
+        dbName: "lireddit",
+        type: "postgresql",
+        debug: !__prod__
+    });
+
+    const post = orm.em.fork({}).create(Post, { title: "my first post" });
+    await orm.em.persistAndFlush(post);
+    console.log("---------slq 2----------")
+    await orm.em.nativeInsert(Post, { title: "my first post" });
+};
+
+main().catch((err) => {
+    console.log(err);
+})
